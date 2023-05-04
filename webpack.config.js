7512614c7bd7f5011736ac661c3e100760beb867
Momo M <momomarcoj@gmail.com>
2023-05-04 18:28:20 +0200

    fixing .env problems

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  // resolve: {
-  //   fallback: {
-  //     fs: false,
-  //     path: false,
-  //     os: false
-  //   }
-  // },
   mode: 'development', // maggiori info di debug
   entry: {
     bundle: path.resolve(__dirname, 'src/js/index.js'),
@@ -91,5 +84,12 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+      // resolve: {
+  //   fallback: {
+  //     fs: false,
+  //     path: false,
+  //     os: false
+  //   }
+  // },
   ],
 }
